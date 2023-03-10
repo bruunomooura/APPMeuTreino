2c31572b7d269cb5950ec3c50eeb2e0afeb48e61
Douglas Stadulni <stadulnidouglas@gmail.com>
2023-03-10 15:16:11 -0300

    Começamos a criação da tela home. Adicionamos códigos para que tanto a foto do usuário, quanto a barra de progresso e a mensagem de criação de tenham bordas coloridas e arredondadas. Incluímos o botão de login também. Colocamos os ícones na tabbar. Ficaram faltando as constraints dos elementos da tela. Por enquanto, somente o botão de login possuí uma constraint funcionando tanto em tela do SE, quanto na tela do PRO MAX.

--- APPMeuTreino/APPMeuTreino/Home/HomeViewController.swift
+++ APPMeuTreino/APPMeuTreino/Home/HomeViewController.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var BorderHomeUIView: UIView!
+    @IBOutlet weak var ImageUser: UIImageView!
+    @IBOutlet weak var BorderProgressBar: UIView!
+    @IBOutlet weak var ProgressBar: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        // Border PictureImageUser
+        
+        let borderPicture = UIColor(named: "BlueMeuTreino")
+        
+        ImageUser.layer.borderWidth = 1
+                ImageUser.layer.masksToBounds = false
+        ImageUser.layer.borderColor = borderPicture?.cgColor
+                ImageUser.layer.cornerRadius = ImageUser.frame.height/2
+                ImageUser.clipsToBounds = true
+        
+        // Border Progress
+        let borderProgress = UIColor(named: "GrayBorderMeuTreino")
+        
+        BorderProgressBar.layer.borderWidth = 1
+        BorderProgressBar.layer.borderColor = borderProgress?.cgColor
+        
+        BorderProgressBar.clipsToBounds = true
+        BorderProgressBar.layer.cornerRadius = 8
+        
+        
+        // Border Home
+        let borderHome = UIColor(named: "OrangeMeuTreino")
+        
+        BorderHomeUIView.layer.borderWidth = 1
+        BorderHomeUIView.layer.borderColor = borderHome?.cgColor
+        
+        BorderHomeUIView.clipsToBounds = true
+        BorderHomeUIView.layer.cornerRadius = 8
+        
     }
     
 }
