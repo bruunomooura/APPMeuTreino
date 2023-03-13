f383e544e1aff89fe4057d0707070c260051a4a1
Barbara Helen <dsbarbarahelen@gmail.com>
2023-03-12 20:10:08 -0400

    configuraçao de constraints e bordas da tela de mudança de senha.

--- APPMeuTreino/APPMeuTreino/ChangePassword/ChangePasswordViewController.swift
+++ APPMeuTreino/APPMeuTreino/ChangePassword/ChangePasswordViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ChangePasswordViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var newpasswordTextField: UITextField!
+    
+    @IBOutlet weak var repeatpasswordTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    passwordTextField.layer.borderColor = UIColor.orange.cgColor
+    passwordTextField.layer.borderWidth = 1.0
+    newpasswordTextField.layer.borderColor = UIColor.orange.cgColor
+    newpasswordTextField.layer.borderWidth = 1.0
+    repeatpasswordTextField.layer.borderColor = UIColor.orange.cgColor
+    repeatpasswordTextField.layer.borderWidth = 1.0
+    saveButton.layer.cornerRadius = 8
+        
         // Do any additional setup after loading the view.
     }
    
