749e95a5dfa73d85e5b80b777dd8eedee1dc1339
Leonardo Lacerda <leoarlac@gmail.com>
2023-03-14 17:29:45 -0400

    tela de login finalizada, constraints estão ok, mas bom dar uma revisada final

--- APPMeuTreino/APPMeuTreino/Login/LoginViewController.swift
+++ APPMeuTreino/APPMeuTreino/Login/LoginViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.layer.cornerRadius = 10
+        emailTextField.layer.borderWidth = 2
+        emailTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+        
+        passwordTextField.layer.cornerRadius = 10
+        passwordTextField.layer.borderWidth = 2
+        passwordTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+        
+        registerButton.layer.cornerRadius = 10
+        
+        loginButton.layer.cornerRadius = 10
+        
+        self.emailTextField.delegate = self
+        
+        self.passwordTextField.delegate = self
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.emailTextField{
+            self.passwordTextField.becomeFirstResponder()
+        }else{
+            self.passwordTextField.resignFirstResponder()
+        }
+        return true
+    }
 }
