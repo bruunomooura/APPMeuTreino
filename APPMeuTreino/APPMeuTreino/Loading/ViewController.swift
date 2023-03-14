28a65c4645712c146aa3695f7c3290c87a569aec
Leonardo Lacerda <leoarlac@gmail.com>
2023-03-14 17:45:59 -0400

    tela loading finalizada

--- APPMeuTreino/APPMeuTreino/Loading/ViewController.swift
+++ APPMeuTreino/APPMeuTreino/Loading/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UINavigationController {
 
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
