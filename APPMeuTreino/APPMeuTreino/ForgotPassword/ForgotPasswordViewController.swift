e58ec7974bd374866dfbf039b948e7aca05b214e
Leonardo Lacerda <leoarlac@gmail.com>
2023-03-15 19:02:26 -0400

    criação do esboço da tela forgot password

--- APPMeuTreino/APPMeuTreino/ForgotPassword/ForgotPasswordViewController.swift
+++ APPMeuTreino/APPMeuTreino/ForgotPassword/ForgotPasswordViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ForgotPasswordViewController: UIViewController {
+class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
