af399ca2f3a41f52a4597d4ffa1ff41f8047c793
Leonardo Lacerda <leoarlac@gmail.com>
2023-03-17 22:00:25 -0300

    tela de registro finalizada com as constraints ok precisa ainda arrumar alguns ajustes

--- APPMeuTreino/APPMeuTreino/Register/RegisterViewController.swift
+++ APPMeuTreino/APPMeuTreino/Register/RegisterViewController.swift
@@ -7,12 +7,66 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var editPhotoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var confirmPasswordLabel: UILabel!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var signUpConfirmButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nameTextField.layer.cornerRadius = 10
+        nameTextField.layer.borderWidth = 2
+        nameTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+        
+        birthdayTextField.layer.cornerRadius = 10
+        birthdayTextField.layer.borderWidth = 2
+        birthdayTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+        
+        emailTextField.layer.cornerRadius = 10
+        emailTextField.layer.borderWidth = 2
+        emailTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+        
+        passwordTextField.layer.cornerRadius = 10
+        passwordTextField.layer.borderWidth = 2
+        passwordTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+        
+        confirmPasswordTextField.layer.cornerRadius = 10
+        confirmPasswordTextField.layer.borderWidth = 2
+        confirmPasswordTextField.layer.borderColor = UIColor(named: "OrangeMeuTreino")?.cgColor
+                
+        signUpConfirmButton.layer.cornerRadius = 10
+        
+        self.nameTextField.delegate = self
+        self.birthdayTextField.delegate = self
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        self.confirmPasswordTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.nameTextField{
+            self.birthdayTextField.becomeFirstResponder()
+        }else if textField == self.birthdayTextField{
+            self.emailTextField.becomeFirstResponder()
+        }else if textField == self.emailTextField{
+            self.passwordTextField.becomeFirstResponder()
+        }else if textField == self.passwordTextField{
+            self.confirmPasswordTextField.becomeFirstResponder()
+        }else{
+            self.confirmPasswordTextField.resignFirstResponder()
+        }
+        return true
     }
 
 }
