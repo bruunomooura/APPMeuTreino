db8565f727c6cb7f7b20a604ff9386bca9d61673
Douglas Stadulni <stadulnidouglas@gmail.com>
2023-07-05 17:04:37 -0300

    Adicionamos o framework Alamofire no projeto

---
+++ APPMeuTreino/Pods/Target Support Files/Pods-APPMeuTreino-APPMeuTreinoUITests/Pods-APPMeuTreino-APPMeuTreinoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_APPMeuTreino_APPMeuTreinoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_APPMeuTreino_APPMeuTreinoUITests
+@end
---
+++ APPMeuTreino/Pods/Target Support Files/Pods-APPMeuTreino/Pods-APPMeuTreino-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_APPMeuTreino : NSObject
+@end
+@implementation PodsDummy_Pods_APPMeuTreino
+@end
