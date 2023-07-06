47b2411765c56c16b4de1859807b81f11e9902cc
Bruno Moura <brunomoura0509@hotmail.com>
2023-07-06 20:42:41 -0300

    Configuração na tela de mudança de senha para que a alteração da senha seja processada via Firebase.

---
+++ APPMeuTreino/APPMeuTreino/ChangePassword/ChagePasswordViewModel/ChangePasswordViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ChangePasswordViewModel.swift
+//  APPMeuTreino
+//
+//  Created by Bruno Moura on 06/07/23.
+//
+
+import UIKit
+import Firebase
+
+protocol ChangePasswordViewModelProtocol: AnyObject {
+    func changePassword(oldPassword: String, newPassword: String, completion: @escaping (Bool) -> Void)
+}
+
+class ChangePasswordViewModel: ChangePasswordViewModelProtocol {
+    func changePassword(oldPassword: String, newPassword: String, completion: @escaping (Bool) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            completion(false)
+            return
+        }
+        
+        let credential = EmailAuthProvider.credential(withEmail: user.email!, password: oldPassword)
+        
+        user.reauthenticate(with: credential) { (authResult, error) in
+            if let errorChangePassword = error {
+                completion(false)
+                return
+            }
+            
+            user.updatePassword(to: newPassword) { (error) in
+                if let _ = error {
+                    completion(false)
+                } else {
+                    completion(true)
+                }
+            }
+        }
+    }
+}
--- APPMeuTreino/APPMeuTreino/ChangePassword/ChangePasswordViewController.swift
+++ APPMeuTreino/APPMeuTreino/ChangePassword/ChangePasswordViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ChangePasswordViewController: UIViewController {
-
+    
+    private var viewModel: ChangePasswordViewModel = ChangePasswordViewModel()
+    private var loadingViewController: LoadingViewController?
+    private var alert: Alert?
+    
     @IBOutlet weak var changePasswordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var newPasswordTextField: UITextField!
@@ -19,6 +23,7 @@ class ChangePasswordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configChangePasswordView()
+        alert = Alert(controller: self)
         
         self.confirmPasswordTextField.delegate = self
         self.newPasswordTextField.delegate = self
@@ -37,9 +42,32 @@ class ChangePasswordViewController: UIViewController {
         configTextField(textField: passwordTextField)
         configTextField(textField: newPasswordTextField)
         configTextField(textField: confirmPasswordTextField)
-
+        
         saveButton.layer.cornerRadius = 10
         saveButton.isEnabled = false
+    }
+    private func showLoadingScreen() {
+        loadingViewController = LoadingViewController()
+        present(loadingViewController!, animated: true, completion: nil)
+    }
+    
+    private func hideLoadingScreen() {
+        loadingViewController?.dismiss(animated: true, completion: nil)
+        loadingViewController = nil
+    }
+    
+    private func showSuccessAlert() {
+        let alert = UIAlertController(title: "Sucesso!", message: "Senha alterada com sucesso.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            self?.navigationController?.popViewController(animated: true)
+        })
+        present(alert, animated: true, completion: nil)
+    }
+
+    private func showErrorAlert() {
+        let alert = UIAlertController(title: "Erro!", message: "Erro ao alterar a senha, verifique a senha informada e tente novamente.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func tappedBackButton(_ sender: UIButton) {
@@ -47,7 +75,22 @@ class ChangePasswordViewController: UIViewController {
     }
     
     @IBAction func tappedSaveButton(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
+        showLoadingScreen()
+        guard let oldPassword = passwordTextField.text, let newPassword = newPasswordTextField.text else {
+            return
+        }
+        
+        viewModel.changePassword(oldPassword: oldPassword, newPassword: newPassword) { [weak self] (success) in
+            DispatchQueue.main.async {
+                if success {
+                    self?.hideLoadingScreen()
+                    self?.showSuccessAlert()
+                } else {
+                    self?.hideLoadingScreen()
+                    self?.showErrorAlert()
+                }
+            }
+        }
     }
 }
 
@@ -67,9 +110,9 @@ extension ChangePasswordViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         if passwordTextField.hasText && newPasswordTextField.hasText && confirmPasswordTextField.hasText{
         if newPasswordTextField.text == confirmPasswordTextField.text {
-        passwordTextField.layer.borderColor = UIColor.orangeMeuTreino.cgColor
-        newPasswordTextField.layer.borderColor = UIColor.orangeMeuTreino.cgColor
-        confirmPasswordTextField.layer.borderColor = UIColor.orangeMeuTreino.cgColor
+        passwordTextField.layer.borderColor = UIColor.blueMeuTreino.cgColor
+        newPasswordTextField.layer.borderColor = UIColor.blueMeuTreino.cgColor
+        confirmPasswordTextField.layer.borderColor = UIColor.blueMeuTreino.cgColor
         saveButton.isEnabled = true
             } else {
         saveButton.isEnabled = false
@@ -79,7 +122,7 @@ extension ChangePasswordViewController: UITextFieldDelegate {
             } else{
         saveButton.isEnabled = false
         if textField.hasText{
-            textField.layer.borderColor = UIColor.orangeMeuTreino.cgColor
+            textField.layer.borderColor = UIColor.blueMeuTreino.cgColor
             }else{
             textField.layer.borderColor = UIColor.red.cgColor
                 }
